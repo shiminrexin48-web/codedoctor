e34d43a93c71f8ccc33a9e36893ce0d26ad36d56
warm people <shiminrexin48@gmail.com>
2026-07-23 15:13:38 +0800

    chore: add packages/analyzers/src/index.ts

---
+++ packages/analyzers/src/index.ts
@@ -0,0 +1,3 @@
+export { createDefaultAnalyzers } from './registry.js';
+export { maintainabilityAnalyzer } from './maintainabilityAnalyzer.js';
+export { getRuleMetadata, ruleMetadata } from './ruleMetadata.js';
