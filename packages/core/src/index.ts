caa8b335d4ccac3cee3331cbfb7e56017cba5f0c
warm people <shiminrexin48@gmail.com>
2026-07-23 15:13:59 +0800

    chore: add packages/core/src/index.ts

---
+++ packages/core/src/index.ts
@@ -0,0 +1,4 @@
+export { scanProject } from './scanPipeline.js';
+export { assessProjectMaturity } from './projectMaturity.js';
+export { profileProject } from './projectProfile.js';
+export type { ScanProjectOptions } from './scanPipeline.js';
